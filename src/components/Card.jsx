0e7affcd2c2c5c6e724e097ab549930060ce626d
Subham <subhamsingh5638@gmail.com>
2024-07-08 22:27:15 +0530

    removed card;

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,13 +4,7 @@ import { useState } from 'react'
 function Card(){
     return(
         <>
-        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl'>
-            <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "red"}}>Red</button>
-            <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "red"}}>Red</button>
-            <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "red"}}>Red</button>
-            <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "red"}}>Red</button>
-            <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "red"}}>Red</button>
-        </div>
+        
 
         </>  
     )
